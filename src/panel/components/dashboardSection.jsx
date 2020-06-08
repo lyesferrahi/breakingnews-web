72ab2a9a60c52866fc054a243ca02adf32cc2d5a
soslyes@yahoo.ca <soslyes@yahoo.ca>
2020-06-08 11:31:52 -0400

    added affirmations with cors

--- src/panel/components/dashboardSection.jsx
+++ src/panel/components/dashboardSection.jsx
@@ -3,25 +3,31 @@ import { Row, Col, Divider } from 'antd';
 import Users from './users';
 import StatisticAction from './Statistic-Action'
 import Articles from './articles';
+import axios from 'axios';
+
 
 
 class DashboardSection extends Component {
     state = {
         scrollSiz: 0,
-        cars: ["BMW", "Mercedes", "Merco"]
+        cars: ["BMW", "Mercedes", "Merco"],
+        phrase: ''
     }
-    display = () => {
-        let result = [];
-        for (var i = 0; i < 100; i++) {
-            result.push(<br />);
-            result.push(i);
-        }
-        return result;
+
+    async componentDidMount() {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        let { data } = await axios.get(proxyurl + 'https://www.affirmations.dev');
+        console.log(data);
+        const { affirmation } = data;
+        this.setState({ phrase: affirmation })
     }
 
     render() {
         return (
             <div>
+                <Row>
+                    <div style={{ textAlign: "center", fontSize: 60, width: '100%' }}>{this.state.phrase}</div>
+                </Row>
                 <Row>
                     <Col span={12}>
                         <Divider>Users Dashboard</Divider>
